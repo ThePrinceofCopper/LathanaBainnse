3667486c762f4184ca96b49498c199ac0e092f14
ThePrinceofCopper <116286298+ThePrinceofCopper@users.noreply.github.com>
2023-03-18 10:21:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function updateTimer() {
+	// yyyy-MM-dd HH:mm:ss
+  past  = Date.parse("2022-09-17 16:15:57");
+  now     = new Date();
+  diff    = now - past;
+
+  years  = Math.floor( diff / (1000*60*60*24*30*12) );
+  months = Math.floor( diff / (1000*60*60*24*30) );
+  days   = Math.floor( diff / (1000*60*60*24) );
+  hours  = Math.floor( diff / (1000*60*60) );
+  mins   = Math.floor( diff / (1000*60) );
+  secs   = Math.floor( diff / 1000 );
+
+  y = years;
+  M = months - years  * 12;
+  d = days   - months * 30;
+  h = hours  - days   * 24;
+  m = mins   - hours  * 60;
+  s = secs   - mins   * 60;
+  document.getElementById("timer")
+    .innerHTML =
+      '<div><span></span></div><br>'+
+      '<div>' + y + '<span>years</span></div>' +
+      '<div>' + M + '<span>months</span></div>' +
+      '<div>' + d + '<span>days</span></div>' +
+      '<div>' + h + '<span>hours</span></div>' +
+      '<div>' + m + '<span>minutes</span></div>' +
+      '<div>' + s + '<span>seconds</span></div>' ;
+}
+setInterval('updateTimer()', 1000 );
